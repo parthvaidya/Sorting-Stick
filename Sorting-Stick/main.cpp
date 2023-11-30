3087b727ccc9a8df147642b408813ad909e3c6fc
Dhiraj Magdum <dhiraj.magdum@outscal.com>
2023-11-30 13:49:31 +0530

    Added initial services

--- Sorting-Stick/main.cpp
+++ Sorting-Stick/main.cpp
@@ -1,5 +1,17 @@
+#include "Main/GameService.h"
 
 int main()
 {
-	return 0;
+    using namespace Main;
+
+    GameService* game_service = new GameService();
+    game_service->ignite();
+
+    while (game_service->isRunning())
+    {
+        game_service->update();
+        game_service->render();
+    }
+
+    return 0;
 }
